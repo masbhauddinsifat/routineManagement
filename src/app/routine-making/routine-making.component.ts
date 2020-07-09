7a30140d472adb4fc9e827a55e93e4732062ff97
Masbha Uddin <masbhauddinsifat@gmail.com>
2020-07-09 20:50:08 +0600

    remove comment and unnessesary code

--- src/app/routine-making/routine-making.component.ts
+++ src/app/routine-making/routine-making.component.ts
@@ -8,12 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RoutineMakingComponent implements OnInit {
 
-  // endTime: string;
-  // days: string[] = ['sat', 'sun', 'mon', 'tue', 'wes', 'thu'];
+
   classes: number[] = [1, 2, 3, 4];
 
-
-  // routines: { day: string, teacherName: string , class: string, startTime: string}[] = [];
   teacherList: { name: string, subject: string, totalClass: number, totalAssign: { time: string, class: number }[] }[] = [];
   loadClass: { time: string, class: number }[] = [];
   totalClass: number;
@@ -62,24 +59,18 @@ export class RoutineMakingComponent implements OnInit {
           let count = 0;
           this.loadClass.map((load) => {
             count++;
-            // console.log("hello1");
-            // const temp = parseInt(load.time, 10) + 1;
-            if (count !== 4){
+
+            if (count !== 4) {
               temp += 1;
 
-              if (temp === 13){
+              if (temp === 13) {
                 temp = 9;
               }
-              classTime = temp.toString() + ':00';
-            } else{
-              count = 0;
-              // temp -= 2;
-              classTime = temp.toString() + ':00';
-            }
-            // if(temp > 12) {
-            //   classTime = '9:00';
-            // }
 
+            } else {
+              count = 0;
+            }
+            classTime = temp.toString() + ':00';
 
             if (load.class === routineForm.class && load.time === classTime) {
               console.log('class is there');
@@ -88,15 +79,15 @@ export class RoutineMakingComponent implements OnInit {
           });
         }
 
-        if (teacher.totalAssign.length >= 0){
+        if (teacher.totalAssign.length >= 0) {
           teacher.totalAssign.map((assign) => {
             if (assign.time === classTime) {
-                isTeacherFree = false;
+              isTeacherFree = false;
             }
           });
         }
 
-        if (isClassFree && isTeacherFree){
+        if (isClassFree && isTeacherFree) {
           this.loadClass.push({
             time: classTime,
             class: routineForm.class
